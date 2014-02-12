8004359edee618c339ddef265e33f84454a97dde
Marius Vollmer <mvollmer@redhat.com>
2014-02-12 21:44:08 +0200

    Init dashboard.

--- src/web/cockpit-main.js
+++ src/web/cockpit-main.js
@@ -176,7 +176,7 @@ function cockpit_update_machines ()
     if (!cockpit_dbus_client)
         cockpit_select_legacy_client ();
 
-    // XXX - cockpit_dashboard_update_machines ();
+    cockpit_dashboard_update_machines ();
 }
 
 function cockpit_add_machine (address)
